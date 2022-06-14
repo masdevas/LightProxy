af2879d01cbf6ac6e5d35a6b142061f720cadfbe
Andrey Morkovkin <mork.andrey.s.dev@gmail.com>
2022-06-14 22:12:38 +0300

    Sync auto & enable switches

--- client/gnome/extension.js
+++ client/gnome/extension.js
@@ -26,13 +26,40 @@ const Me = ExtensionUtils.getCurrentExtension();
 const Main = imports.ui.main;
 const PanelMenu = imports.ui.panelMenu;
 const PopupMenu = imports.ui.popupMenu;
+const CheckBox = imports.ui.checkBox;
 
 class Extension {
     constructor() {
         this._indicator = null;
     }
+
+    setAutoModeToAutoSwitch() {
+        const actual_mode = this.proxySetting.get_string('mode')
+        if (actual_mode.localeCompare('auto') === 0) {
+            this.autoProxySwitch._switch.state = true;
+        } else {
+            this.autoProxySwitch._switch.state = false;
+        }
+    }
     
+    setEnableModeToEnableSwitch() {
+        const actual_mode = this.proxySetting.get_string('mode')
+        if (actual_mode.localeCompare('none') === 0) {
+            this.enableProxySwitch._switch.state = false;
+            this.autoProxySwitch.setSensitive(false)
+        } else if (actual_mode.localeCompare('manual') === 0 || actual_mode.localeCompare('auto') === 0) {
+            this.enableProxySwitch._switch.state = true;
+            this.autoProxySwitch.setSensitive(true)
+        }
+        this.setAutoModeToAutoSwitch();
+    }
+
+    // setActualModeToSetting() {
+    //     const actual_mode = 
+    // }
+
     enable() {
+        const extension_this = this
         log(`enabling ${Me.metadata.name}`);
 
         let indicatorName = `${Me.metadata.name} Indicator`;
@@ -41,56 +68,63 @@ class Extension {
         this._indicator = new PanelMenu.Button(0.0, indicatorName, false);
         
         // Add an icon
-        log(`Helo ###`)
         // let icon = new St.Icon({Gio.icon_new_for_string(Me.path + "/icons/my_icon.png")});
-        log(`Helo2 ###`)
-        log('###'+Me.path + "/icons/my_icon.png")
-        log(`${Gio.ThemedIcon.names}`)
         let icon = new St.Icon({
             gicon: Gio.icon_new_for_string(Me.path + "/icons/lproxy.png"),
             style_class: 'system-status-icon'
         });
         this._indicator.add_child(icon);
-        log(`Helo3 ###`)
 
         // `Main.panel` is the actual panel you see at the top of the screen,
         // not a class constructor.
         Main.panel.addToStatusArea(indicatorName, this._indicator);
 
         let dndMenu = this._indicator.menu;
-        log(`HELO4 $$$`)
 
-        let switchDNDitem = new PopupMenu.PopupSwitchMenuItem(
+        this.proxySetting = new Gio.Settings({ schema: 'org.gnome.system.proxy' });
+        
+        /* Auto switch configuration */
+        this.autoProxySwitch = new PopupMenu.PopupSwitchMenuItem(
+            "Auto",
+            false,
+        );
+        this.setAutoModeToAutoSwitch()
+        var set_auto_mode_to_auto_switch_handler = function() {
+            log("# Auto mode changed");
+            extension_this.setAutoModeToAutoSwitch()
+        }
+        this.proxySetting.connect("changed", set_auto_mode_to_auto_switch_handler)
+
+        /* Enable proxy switch configuration */
+        this.enableProxySwitch = new PopupMenu.PopupSwitchMenuItem(
             "Enable Proxy",
             false,
         );
+        this.setEnableModeToEnableSwitch()
+        var setting_to_switch_func = function() {
+            log("# Proxy mode changed");
+            extension_this.setEnableModeToEnableSwitch()
+        }
+        this.proxySetting.connect("changed", setting_to_switch_func);
+        
+        // var switch_to_setting_func = function() {
+        //     log("# Enable switch toggled");
+        //     extension_this.setActualModeToSetting()
+        // }
+        // this.enableProxySwitch.connect("toggled", switch_to_setting_func)
 
-        this.proxySetting = new Gio.Settings({ schema: 'org.gnome.system.proxy' });
-        let proxySetting = this.proxySetting
-        // log(Gio.Settings.list_schemas())
-        function setActualMode(proxySetting) {
-            const actual_mode = proxySetting.get_string('mode')
-            // log('&&&&&'+typeof actual_mode)
-            // log(actual_mode.localeCompare('none'))
-            // log(actual_mode === 'none')
-            if (actual_mode.localeCompare('none') === 0) {
-                log(`?? NONE`)
-                switchDNDitem._switch.state = false;
-            } else if (actual_mode.localeCompare('manual') === 0 || actual_mode.localeCompare('auto') === 0) {
-                log(`?? MANUAL`)
-                switchDNDitem._switch.state = true;
-            }
-            // log('&^^ ACTUAL'+actual_mode)
-        }
-        setActualMode(proxySetting)
-        var proxy_connect_func = function() {
-            log("I see, you changed it!");
-            // proxySetting = new Gio.Settings({ schema: 'org.gnome.system.proxy' });
-            setActualMode(proxySetting)
-        }
-        // proxy_connect_func.setActualMode = setActualMode
-        // proxy_connect_func.proxySetting = this.proxySetting
-        proxySetting.connect("changed", proxy_connect_func);
+        //this.checkBoxAutoProxy = new CheckBox.CheckBox('label')
+        // log(Object.keys(this.switchProxyEnabledItem))
+        // log(typeof this.switchProxyEnabledItem._delegate)
+        // log(this.switchProxyEnabledItem._parent)
+        // log(Object.keys(this.switchProxyEnabledItem._statusBin))
+        // log(typeof this.switchProxyEnabledItem._active)
+        // log(typeof this.switchProxyEnabledItem._activatable)
+        // log(Object.keys(this.switchProxyEnabledItem._box))
+        // this.switchProxyEnabledItem._sensitive = false
+        // this.switchProxyEnabledItem._active = false
+        // this.switchProxyEnabledItem._activatable = false
+
         /* this.proxySetting.conn
         actual_mode = 
         this.proxySettingHttp = new Gio.Settings({ schema: 'org.gnome.system.proxy.http' });
@@ -100,8 +134,13 @@ class Extension {
         log(this.proxySettingHttps.get_int('port'))
         log(this.proxySettingHttp.get_string('host'))
         log(this.proxySettingHttp.get_int('port')) */
-        dndMenu.addMenuItem(switchDNDitem);
-
+        // this.switchProxyEnabledItem.setSensitive(false)
+        dndMenu.addMenuItem(this.autoProxySwitch);
+        dndMenu.addMenuItem(this.enableProxySwitch);
+        // dndMenu.add_child(this.checkBoxAutoProxy);
+        // this.switchProxyEnabledItem.disable();
+        // log(Object.keys(this.switchProxyEnabledItem))
+        // this.switchProxyEnabledItem._active = false
     }
     
     // REMINDER: It's required for extensions to clean up after themselves when
