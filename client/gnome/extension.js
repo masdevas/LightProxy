8c11ecc6930d388ceba302f6a68c8639e29ef0b3
Andrey Morkovkin <mork.andrey.s.dev@gmail.com>
2022-06-14 19:55:30 +0300

    Proxy toggle synchronized with actual proxy mode

--- client/gnome/extension.js
+++ client/gnome/extension.js
@@ -25,7 +25,7 @@ const ExtensionUtils = imports.misc.extensionUtils;
 const Me = ExtensionUtils.getCurrentExtension();
 const Main = imports.ui.main;
 const PanelMenu = imports.ui.panelMenu;
-
+const PopupMenu = imports.ui.popupMenu;
 
 class Extension {
     constructor() {
@@ -52,9 +52,56 @@ class Extension {
         });
         this._indicator.add_child(icon);
         log(`Helo3 ###`)
+
         // `Main.panel` is the actual panel you see at the top of the screen,
         // not a class constructor.
         Main.panel.addToStatusArea(indicatorName, this._indicator);
+
+        let dndMenu = this._indicator.menu;
+        log(`HELO4 $$$`)
+
+        let switchDNDitem = new PopupMenu.PopupSwitchMenuItem(
+            "Enable Proxy",
+            false,
+        );
+
+        this.proxySetting = new Gio.Settings({ schema: 'org.gnome.system.proxy' });
+        let proxySetting = this.proxySetting
+        // log(Gio.Settings.list_schemas())
+        function setActualMode(proxySetting) {
+            const actual_mode = proxySetting.get_string('mode')
+            // log('&&&&&'+typeof actual_mode)
+            // log(actual_mode.localeCompare('none'))
+            // log(actual_mode === 'none')
+            if (actual_mode.localeCompare('none') === 0) {
+                log(`?? NONE`)
+                switchDNDitem._switch.state = false;
+            } else if (actual_mode.localeCompare('manual') === 0 || actual_mode.localeCompare('auto') === 0) {
+                log(`?? MANUAL`)
+                switchDNDitem._switch.state = true;
+            }
+            // log('&^^ ACTUAL'+actual_mode)
+        }
+        setActualMode(proxySetting)
+        var proxy_connect_func = function() {
+            log("I see, you changed it!");
+            // proxySetting = new Gio.Settings({ schema: 'org.gnome.system.proxy' });
+            setActualMode(proxySetting)
+        }
+        // proxy_connect_func.setActualMode = setActualMode
+        // proxy_connect_func.proxySetting = this.proxySetting
+        proxySetting.connect("changed", proxy_connect_func);
+        /* this.proxySetting.conn
+        actual_mode = 
+        this.proxySettingHttp = new Gio.Settings({ schema: 'org.gnome.system.proxy.http' });
+        this.proxySettingHttps = new Gio.Settings({ schema: 'org.gnome.system.proxy.https' });
+        log(this.proxySetting.get_string('mode'))
+        log(this.proxySettingHttps.get_string('host'))
+        log(this.proxySettingHttps.get_int('port'))
+        log(this.proxySettingHttp.get_string('host'))
+        log(this.proxySettingHttp.get_int('port')) */
+        dndMenu.addMenuItem(switchDNDitem);
+
     }
     
     // REMINDER: It's required for extensions to clean up after themselves when
